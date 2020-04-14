855fd11343f77206c7187cfc2f5217b80e52a5cc
Tiago Luiz dos Santos <tiagoluizd@gmail.com>
2020-04-13 22:12:18 -0300

    Update Login.py

--- Login.py
+++ Login.py
@@ -1,3 +1,16 @@
+#Import Selenium Dependencies
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+import chromedriver_binary  # Adds chromedriver binary to path
+import os
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+driver = webdriver.Chrome()
+driver.get("https://pt.quora.com/")
+time.sleep(2)
 #Login to Quora
 def LoginPage():
     #Credentials
